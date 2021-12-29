049fd45cc43e6ecfcb379fae932966cee3c47b54
Bogdan-Adrian Vidrașcu <60898167+BVidrascu@users.noreply.github.com>
2021-12-29 22:11:19 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,21 @@
 import streamlit as st
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+import pandas as pd
+
+data = pd.read_csv('survey_results_public.csv')
+header = st.container()
+body = st.container()
+
+with header:
+    st.title('Bine ai venit, prost ai nimerit')
+    st.text('Aici o sa vezi si nu o sa crezi, crede-ma pe mine bro')
+
+with body:
+    st.text('Hai prima data sa vedem cu ce lucram!')
+    st.write(data.head())
+    st.text('Destul de nice, nu?\nAnyways, uite un chart care arata distributia adeptilor culturii open source dintre respondenti')
+    opsrc_distribution = pd.DataFrame(data['OpenSourcer'].value_counts())
+    st.bar_chart(opsrc_distribution, height = 700, width = 1000)
+    x = st.slider('Cat de mult ti-a placut?', 1, 10, 0)  
+    st.write('Ok, ', x, ' sa fie, daca atata vrei tu...')
+    if st.button('Nu apasa butonul'):
+     st.balloons()
