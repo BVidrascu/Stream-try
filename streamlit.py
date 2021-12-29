8c9e89401c4c07d4cbfd35ea47b42a28d2935faf
Bogdan-Adrian Vidrașcu <60898167+BVidrascu@users.noreply.github.com>
2021-12-29 22:30:05 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,7 +12,8 @@ with header:
 with body:
     st.text('Hai prima data sa vedem cu ce lucram!')
     st.write(data.head())
-    st.text('Destul de nice, nu?\nAnyways, uite un chart care arata distributia adeptilor culturii open source dintre respondenti')
+    st.markdown('Destul de nice, nu?')
+    st.markdown('Anyways, uite un chart care arata distributia adeptilor culturii open source dintre respondenti')
     opsrc_distribution = pd.DataFrame(data['OpenSourcer'].value_counts())
     st.bar_chart(opsrc_distribution, height = 700, width = 1000)
     x = st.slider('Cat de mult ti-a placut?', 1, 10, 0)  
